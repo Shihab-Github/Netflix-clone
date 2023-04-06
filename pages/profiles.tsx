69c575cae6652c0123b81840942d2639b1fc2493
Farzana Sultana <farzana.sultana@bongobd.com>
2023-04-07 03:46:48 +0600

    profile screen

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -1,28 +1,69 @@
-import { NextPageContext } from "next"
-import { getSession } from "next-auth/react"
+import useCurrentUser from '@/hooks/useCurrentUser'
+import { NextPageContext } from 'next'
+import { getSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
 
-export async function getServerSideProps(context:NextPageContext) {
-    const session = getSession(context)
+export async function getServerSideProps(context: NextPageContext) {
+  const session = getSession(context)
 
-    if(!session) {
-        return {
-            redirect: {
-                destination: '/auth'
-            }
-        }
-    }
-
+  if (!session) {
     return {
-        props: {}
+      redirect: {
+        destination: '/auth'
+      }
     }
+  }
+
+  return {
+    props: {}
+  }
 }
 
 const Profiles = () => {
-    return (
-        <div>
-            <p className="text-4xl text-white">Profiles</p>
+  const router = useRouter()
+  const { data: user } = useCurrentUser()
+  return (
+    <div className="flex items-center h-full justify-center">
+      <div className="flex flex-col">
+        <h1 className="text-3xl md:text-6xl text-white text-center">
+          Who is watching ?
+        </h1>
+        <div
+          onClick={() => router.push('/')}
+          className="flex items-center justify-center gap-8 mt-10"
+        >
+          <div className="group flex-row w-44 mx-auto">
+            <div
+              className="
+                    w-44
+                    h-44
+                    rounded-md
+                    flex
+                    items-center
+                    justify-center
+                    border-2
+                    border-transparent
+                    group-hover:cursor-pointer
+                    group-hover:border-white
+                    overflow-hidden"
+            >
+              <img src="/images/default-green.png" alt="Profile" />
+            </div>
+            <div
+              className="
+                                mt-4
+                                text-gray-400
+                                text-center
+                                group-hover:text-white
+                            "
+            >
+              {user?.name}
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Profiles
